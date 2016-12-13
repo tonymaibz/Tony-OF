67ff704510d71225597717255ce34087dc8a3bde
ty_tony <ty_tony@qq.com>
2016-12-13 21:08:05 +0800

    删除目录dashboard

--- dashboard/rrd/consts.py
+++
@@ -1,8 +0,0 @@
-#-*-coding:utf8-*-
-
-RRD_CFS = ['AVERAGE', 'MAX', 'MIN', 'LAST']
-
-GRAPH_TYPE_KEY = 'k'
-GRAPH_TYPE_HOST = 'h'
-ENDPOINT_DELIMITER = "|"
-COUNTER_DELIMITER = "|"
--- dashboard/rrd/view/index.py
+++
@@ -1,7 +0,0 @@
-#-*- coding:utf-8 -*-
-from flask import render_template
-from rrd import app
-
-@app.route("/")
-def index():
-    return render_template("index.html", **locals())
