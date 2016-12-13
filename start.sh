ffd014122b9ed087abf2d00126eb3b58be66ab13
Tony Mak <ty_tony@qq.com>
2016-12-13 21:03:05 +0800

    Open-Falcon 深圳服务器启动脚本

---
+++ start.sh
@@ -0,0 +1,14 @@
+/root/work/tmp/transfer/control start
+/root/work/tmp/agent/control start
+/root/work/tmp/query/control start
+/root/work/tmp/graph/control start
+/root/work/tmp/mail-provider-master/control start
+/root/work/tmp/sender/control start
+/root/work/tmp/fe/control start
+/root/work/tmp/portal/control start
+/root/work/tmp/hbs/control start
+/root/work/tmp/judge/control start
+/root/work/tmp/alarm/control start
+/root/work/tmp/links/control start
+
+service grafana-server start
